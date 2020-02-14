1995cbb4f74fe4dc6841b25014700b080b7b6d5f
Martina Gabrielli <martina.gabrielli@yahoo.it>
2020-02-14 18:39:53 +0000

    created home page template, images upload and styling update

---
+++ wp-content/themes/SG/page-59.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap cf">
+
+						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+							<div class="">
+
+
+							</div>
+
+							<?php endwhile; endif; ?>
+
+						</main>
+
+				</div>
+
+			</div>
+
+<?php get_footer(); ?>
