4ec8842fdd7bca7167fbad8effd2b76c8e360aca
Martina Gabrielli <martina.gabrielli@yahoo.it>
2020-03-16 15:17:20 +0000

    added shop article custom field and style

--- wp-content/themes/SG/page-59.php
+++ wp-content/themes/SG/page-59.php
@@ -14,6 +14,22 @@
                                 <?php endif; ?>
                             </div>
 
+                            <div class="shop-article">
+                                <h2><?php the_field('shop_title'); ?></h2>
+                                <div class="shop-wrap">
+                                    <?php 
+                                    $link = get_field('shop_link');
+                                    if( $link ): 
+                                        $link_url = $link['url'];
+                                        $link_title = $link['title'];
+                                        $link_target = $link['target'] ? $link['target'] : '_self';
+                                        ?>
+                                        <a class="shop-link" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
+                                    <?php endif; ?>
+                                </div>
+                                <img class="background-img" class="shop-img" src="<?php the_field('shop_image'); ?>">
+                            </div>
+
 						</main>
 
 				</div>
